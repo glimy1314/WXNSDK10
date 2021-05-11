1aa5f6fe464a8f75e09ba8b88a87dd8535d6cbaf
guoliang <guoliang@baiing.cn>
2021-05-11 15:24:42 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-WXNSDK10_Example/Pods-WXNSDK10_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WXNSDK10_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WXNSDK10_Example
+@end
---
+++ Example/Pods/Target Support Files/WXNSDK10/WXNSDK10-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WXNSDK10 : NSObject
+@end
+@implementation PodsDummy_WXNSDK10
+@end
